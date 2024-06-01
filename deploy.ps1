afa77a4d3a0aaca3a2f32f0ab9c90edeb927368f
Jeferson Tenorio <jefersonsv@gmail.com>
2024-06-01 20:27:43 +0100

    error

--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,7 @@
+$env:PNPM_FLAGS = "--shamefully-hoist"
 rimraf node_modules
 rimraf pnpm-lock.yaml
-pnpm i
+rimraf .next
+pnpm install --shamefully-hoist
 netlify build --context deploy-preview
 netlify deploy --dir=.netlify/static --functions=.netlify/functions
