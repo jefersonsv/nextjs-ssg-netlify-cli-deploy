768951a7abf39e625fd17eab06eb38685661d672
Jeferson Tenorio <jefersonsv@gmail.com>
2024-06-01 19:46:18 +0100

    pnpm error

--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,5 @@
 rimraf node_modules
-rimraf yarn.lock
-yarn
+rimraf pnpm-lock.yaml
+pnpm i
 netlify build --context deploy-preview
 netlify deploy --dir=.netlify/static --functions=.netlify/functions
