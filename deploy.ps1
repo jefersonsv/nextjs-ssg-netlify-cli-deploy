cb95b708742c9eb743eaaa04136a10e26ecb6fad
Jeferson Tenorio <jefersonsv@gmail.com>
2024-06-01 21:11:39 +0100

    erro

--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,6 @@
-rimraf node_modules
-rimraf yarn.lock
+rimraf apps/web/node_modules
+rimraf apps/web/.next
+rimraf apps/web/yarn.lock
 yarn
 netlify build --context deploy-preview
 netlify deploy --dir=.netlify/static --functions=.netlify/functions
